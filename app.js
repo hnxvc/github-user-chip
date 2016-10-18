2361525edd5cb45f84a30a688f99d41d39dd230d
Hoa Xuan <hoaxuan@brodev.com>
2016-10-18 21:59:32 +0700

    Add feature click x icon to delete

--- app.js
+++ app.js
@@ -21,15 +21,6 @@ class UserInput extends React.Component{
     this.props.handleAddUser(username);
   }
 
-  onDeleteClick(){
-
-    let eUserName = ReactDOM.findDOMNode(this.refs.username);
-    let username = eUserName.value;
-    eUserName.value = '';
-
-    this.props.handleDeleteUser(username);
-  }
-
   render(){
     return (
       <div className="mdl-cell mdl-cell--12-col">
@@ -40,37 +31,26 @@ class UserInput extends React.Component{
         <button onClick={this.onAddClick.bind(this)} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect">
           Add
         </button>
-        <button onClick={this.onDeleteClick.bind(this)} className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
-          Delete
-        </button>
       </div>
     );
   }
 }
 
 
-
-class User extends React.Component{
-  onDeleteUser(e){
-    console.log(e.target.id)
-  }
-  render(){
-    return(
-      <span className="mdl-chip mdl-chip--contact mdl-chip--deletable">
-        <a href={this.props.user.html_url} ><img className="mdl-chip__contact" src={this.props.user.avatar_url} /> </a>
-        <span className="mdl-chip__text">{this.props.user.name}</span>
-        <span className="mdl-chip__action"><i id={this.props.user.login} onClick={this.onDeleteUser.bind(this)} className="material-icons">cancel</i></span>
-      </span>
-    );
-  }
-}
-
-
-
 class ListUser extends React.Component{
 
+  onDeleteUser(e){
+    this.props.onDeleteUser(e.target.id);
+  }
+
   render(){
-    let users = this.props.users.map((user,id) => <User key={id} user={user} />);
+    let users = this.props.users.map((user,id) => (
+      <span key={id} className="mdl-chip mdl-chip--contact mdl-chip--deletable">
+        <a href={user.html_url} target="_blank" ><img className="mdl-chip__contact" src={user.avatar_url} /> </a>
+        <span className="mdl-chip__text">{user.name}</span>
+        <span className="mdl-chip__action"><i id={id} onClick={this.onDeleteUser.bind(this)} className="material-icons">cancel</i></span>
+      </span>
+    ));
     return(
       <div className="mdl-cell mdl-cell--12-col">
           {users}
@@ -98,20 +78,14 @@ class App extends React.Component{
       users.push(data);
       that.setState({users:users});
     }).fail(function() {
-      alert( "Data not found !" );
+      alert("Data not found !");
     })
   }
 
-  deleteUser(username){
 
+  deleteUser(id){
     let users = this.state.users;
-    
-    for(var i =0;i<users.length; i++){
-      if(users[i].login === username){
-        users.splice(i,1);
-        break;
-      }
-    }
+    users.splice(id,1);
     this.setState({users:users});
   }
 
@@ -119,8 +93,8 @@ class App extends React.Component{
     return (
       <div className="mdl-grid">
         <Title title="Github user chip" /> 
-        <UserInput handleAddUser={this.addNewUser.bind(this)} handleDeleteUser={this.deleteUser.bind(this)} />
-        <ListUser users={this.state.users} />
+        <UserInput handleAddUser={this.addNewUser.bind(this)} />
+        <ListUser users={this.state.users} onDeleteUser={this.deleteUser.bind(this)} />
       </div>
     );
   }
